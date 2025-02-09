62e30fb52dab7aea0ba0da61affe78e5db42d1f5
Krisa29 <sathvika.ganni@gmail.com>
2025-02-09 09:28:26 -0500

    Imported add and subtract functions

---
+++ tests/test_calculator.py
@@ -0,0 +1,11 @@
+'''Calculator Test'''
+from calculator import add, subtract
+
+def test_addition():
+    '''Test that addition function works '''    
+    assert add(5,2) == 7
+
+def test_subtraction():
+    '''Test that subtraction function works '''    
+    assert subtract(3,2) == 1
+    
