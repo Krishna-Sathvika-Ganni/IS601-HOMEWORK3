b3bea7f1d2da8cecc3431b4cd7bd3f12d0ebea95
Krisa29 <sathvika.ganni@gmail.com>
2025-02-09 19:38:25 -0500

    Imported multiply and divide functions

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -9,4 +9,12 @@ def test_subtraction():
     '''Test that subtraction function works '''    
     assert subtract(3,2) == 1
 
+def test_multiplication():
+    '''Test that Multiplication function works'''
+    assert multiply(3,4) == 12
+
+def test_division():
+    '''Test that Division function works'''
+    assert divide(10,5) == 2
+
 #End
