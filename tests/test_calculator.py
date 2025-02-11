88c343c9ea3e48d180272b0f39077dcffcebaff9
Krisa29 <sathvika.ganni@gmail.com>
2025-02-10 21:26:38 -0500

    Fixed a not defined error

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -3,18 +3,18 @@ from calculator import Calculator
 
 def test_addition():
     '''Test that addition function works '''    
-    assert add(5,2) == 7
+    assert Calculator.add(5,2) == 7
 
 def test_subtraction():
     '''Test that subtraction function works '''    
-    assert subtract(3,2) == 1
+    assert Calculator.subtract(3,2) == 1
 
 def test_multiplication():
     '''Test that Multiplication function works'''
-    assert multiply(3,4) == 12
+    assert Calculator.multiply(3,4) == 12
 
 def test_division():
     '''Test that Division function works'''
-    assert divide(10,5) == 2
+    assert Calculator.divide(10,5) == 2
 
 #End
