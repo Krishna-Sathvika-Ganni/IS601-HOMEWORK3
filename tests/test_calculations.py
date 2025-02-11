d847fcf4048923bfe24cd575fc547a8913293b28
Krisa29 <sathvika.ganni@gmail.com>
2025-02-10 21:18:31 -0500

    Saved the importing of functions code in test_calculations file

---
+++ tests/test_calculations.py
@@ -0,0 +1,20 @@
+'''Calculator Test'''
+from calculator.operations import add, subtract, multiply, divide
+
+def test_addition():
+    '''Test that addition function works '''    
+    assert add(5,2) == 7
+
+def test_subtraction():
+    '''Test that subtraction function works '''    
+    assert subtract(3,2) == 1
+
+def test_multiplication():
+    '''Test that Multiplication function works'''
+    assert multiply(3,4) == 12
+
+def test_division():
+    '''Test that Division function works'''
+    assert divide(10,5) == 2
+
+#End
