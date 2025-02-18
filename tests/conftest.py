b1662eda0e08d761b8a0ef239f22312c9d669455
Krisa29 <sathvika.ganni@gmail.com>
2025-02-18 17:44:29 -0500

    Fixed undefined variable error

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 '''Configuration file to define fixtures and other configurations'''
 
 from decimal import Decimal
+import pytest
 from faker import Faker
 from calculator.operations import add, subtract, multiply, divide
 
@@ -41,6 +42,6 @@ def generate_test_data(num_records):
     def pytest_generate_tests(metafunc):
         if {"x", "y", "expected"}.intersection(set(metafunc.fixturenames)):
             num_records = metafunc.config.getoption("num_records")
-            parameters = list(generating_test_data(num_records))
+            parameters = list(generate_test_data(num_records))
             modified_parameters = [(x, y, op_name if 'operation_name' in metafunc.fixturenames else op_func, expected) for x, y, op_name, op_func, expected in parameters]
             metafunc.parametrize("x,y,operation,expected", modified_parameters)
