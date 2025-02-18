14280ca68ddc021a764b5fdd4bbc09c3163f58a6
Krisa29 <sathvika.ganni@gmail.com>
2025-02-18 17:30:50 -0500

    Fixed errors in the conftest file

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,12 @@
+'''Configuration file to define fixtures and other configurations'''
+
 from decimal import Decimal
 import pytest
+from faker import Faker
 from calculator.operations import add, subtract, multiply, divide
 
-from faker import Faker
-
-fake = Faker() # Initalizing the faker object
+fake = Faker() 
+'''Initalizing the faker object'''
 
 def generating_test_data(num_records):
     operation_mappings = {
@@ -20,22 +22,21 @@ def generating_test_data(num_records):
         operation_name = fake.random_element(elements=list(operation_mappings.keys()))
         operation_func = operation_mappings[operation_name]
 
-        if operation_func == divide:
+        if operation_func is divide:
             y = Decimal('1') if y == Decimal('0') else y  
         try:
-            if operation_func == divide and y == Decimal('0'):
+            if operation_func is divide and y == Decimal('0'):
                 expected = "ZeroDivisionError"
             else:
                 expected = operation_func(x,y)
         except ZeroDivisionError:
             expected = "ZeroDivisionError"
-        
         yield x, y, operation_name, operation_func, expected
 
     def pytest_addoption(parser):
         parser.addoption("--num_records", action="store",default=5, type=int, help="Number of test records to generate")
 
-    def pytest_generating_tests(metafunc):
+    def pytest_generate_tests(metafunc):
         if {"x", "y", "expected"}.intersection(set(metafunc.fixturenames)):
             num_records = metafunc.config.getoption("num_records")
             parameters = list(generating_test_data(num_records))
