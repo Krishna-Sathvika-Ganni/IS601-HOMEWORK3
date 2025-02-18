94c31fa86fa48d23923c0cf15afb1761287599c5
Krisa29 <sathvika.ganni@gmail.com>
2025-02-18 18:45:22 -0500

    Corrected errors

--- tests/test_main.py
+++ tests/test_main.py
@@ -6,7 +6,7 @@ from main import calculate_and_print
     ("4", "1", 'subtract', "The result of 4 subtract 1 is equal to 3"),
     ("2", "2", 'multiply', "The result of 2 multiply 2 is equal to 4"),
     ("10", "5", 'divide', "The result of 10 divide 5 is equal to 2"),
-    ("4", "0", 'divide', "An error occurred: Cannot be divided by zero"),  
+    ("4", "0", 'divide', "An error occurred: Cannot be divided by Zero"),  
     ("6", "3", 'unknown', "Unknown operation: unknown"),  
     ("x", "3", 'add', "Invalid number input: x or 3 is not a valid number."),  
     ("5", "y", 'subtract', "Invalid number input: 5 or y is not a valid number.")  
